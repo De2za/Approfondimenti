181a97263befa3a2e0eb94771b60e201f5e22a5f
CeleghinRiccardo <celeghin.riccardo@studenti.salesianisesto.it>
2026-02-15 16:38:23 +0100

    s

--- 1Q/repo-merge-issuies-TDD/src/Calcolatrice.java
+++ 1Q/repo-merge-issuies-TDD/src/Calcolatrice.java
@@ -15,11 +15,4 @@ public class Calcolatrice {
         }
         return Math.sqrt(x);
     }
-
-    public double sum(double a, double b) throws ArithmeticException {
-        if (b == integer.MAX_VALUE ) {
-            throw new ArithmeticException("Divisione per zero");
-        }
-        return a + b;
-    }
 }
